89c16840d94d4a6fe43c77a94b321456af779435
petziferum <daboarderpjb@gmail.com>
2019-11-11 14:05:04 +0100

    bilder

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,23 @@
 <template>
   <v-container>
-    <v-card class="pa-4">
-      <v-card-title>Trade or View your Portfolio</v-card-title>
-      You may save or Load you Data<br>
-      <hr>
-      <p>Your Funds: {{ funds | currency }}</p>
+    <v-card class="pa-0 elevation-10" hover="elevation-2">
+      <v-img class="white--text align-end ma-0" height="200px" src="../assets/bgimg.jpg">
+        <v-card-title>Trade or View your Portfolio</v-card-title>
+        <v-card-subtitle class="white--text">You may save or Load you Data</v-card-subtitle>
+        <br />
+        <hr />
+        <v-card-title primary-title>
+          <p>Your Funds: {{ funds | currency }}</p>
+        </v-card-title>
+      </v-img>
     </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
- 
-
   data: () => ({
-
+    bgImage: require("../assets/bgimg.jpg")
   }),
   computed: {
     funds() {
@@ -23,3 +26,5 @@ export default {
   }
 };
 </script>
+<style scoped>
+</style>
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,26 +1,28 @@
 <template>
   <v-card class="ma-2" width="600px">
-    <v-card-title class="success pa-1">
-      <v-list-item two-line>
-        <v-list-item-content>
-          <v-list-item-title class="headline">{{ stock.name }}</v-list-item-title>
-          <v-list-item-subtitle>{{stock.price}}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card-title>
+    <v-img src="../../assets/bgimg.jpg" max-height="100">
+      <v-card-title class="pa-1 white--text">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline white--text">{{ stock.name }}</v-list-item-title>
+            <v-list-item-subtitle class="white--text">{{stock.price}}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card-title>
+    </v-img>
 
     <v-container>
       <v-row>
-        <v-col>
+        <v-col cols="12">
           <input
-            d-inline-flex
             type="number"
-            class="form-control"
             placeholder="Quantity"
             v-model="quantity"
             :class="{danger: insufficientFunds}"
           />
         </v-col>
+      </v-row>
+      <v-row>
         <v-col cols="3">
           <v-btn @click="buyStock" tile :disabled="disableButton()">Buy</v-btn>
         </v-col>
